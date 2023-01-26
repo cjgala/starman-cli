95a20270738a0aa15ed5d305af582dba5afd65f5
Chuck Gala <cjgala@gmail.com>
2023-01-26 16:31:55 -0500

    Add render method for basic auth header

--- spaceman/render.py
+++ spaceman/render.py
@@ -1,3 +1,4 @@
+import base64
 import uuid
 
 from jinja2 import Template, Undefined
@@ -9,7 +10,12 @@ def render_template(text, params):
    template = Template(text, undefined=SilentUndefined)
    template.globals["increment"] = lambda x: int(x) + 1
    template.globals["random_uuid"] = lambda: uuid.uuid4()
+   template.globals["basic_auth"] = basic_auth
    return template.render(params)
+
+def basic_auth(username, password):
+  data = bytes(username + ":" + password, "utf-8")
+  return "Basic " + base64.b64encode(data).decode("utf-8")
 
 class SilentUndefined(Undefined):
     def _fail_with_undefined_error(self, *args, **kwargs):
